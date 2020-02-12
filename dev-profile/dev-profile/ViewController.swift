00fbff5884d24c639623764e970c6a9f86489251
Sterling Freeman <joseph.freeman1234@gmail.com>
2020-02-12 16:46:38 +0000

    App front end example that showcases grasp of class sizing in iOS/Swift/XCode

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Joseph Freeman on 2020-02-11.
+//  Copyright © 2020 Sterling Freeman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
