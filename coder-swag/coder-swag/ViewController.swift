8b17fab9e0a093a1fd944637e7a3cf8ca986caab
Sterling Freeman <joseph.freeman1234@gmail.com>
2020-02-17 07:35:21 +0000

    First Commit. Empty project

---
+++ coder-swag/coder-swag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Joseph Freeman on 2020-02-17.
+//  Copyright © 2020 Sterling Freeman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
